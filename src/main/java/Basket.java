f613ae500c6a0834143d712de2e8483cefc5adeb
Qwexaro <Sergikstar25.gold@mail.ru>
2024-11-15 14:06:29 +0300

    main

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -3,11 +3,11 @@ public class Basket {
     private int totalPrice = 0;
     private int limit = 1_000_000;
     private static int countBasket = 0;
+    private double totalWeight = 0;
 
-
-    public Basket(String items, int totalPrice) {
+    public Basket(String items, int totalPrice, double totalWeight) {
         this();
-        add(items, totalPrice);
+        add(items, totalPrice, totalWeight);
     }
 
     public Basket(int limit) {
@@ -21,28 +21,33 @@ public class Basket {
         items += "Список товаров:";
     }
 
-    public void add(String name, int price, int count) {
+    public void add(String name, int price, int count, double weight) {
+
         price *= count;
         if (totalPrice + price > limit) {
             return;
         } else if (containsItem(name)) {
             return;
         } else if (items.isEmpty()) {
-            items += name + " - " + price;
+            items += name + " - " + price + " Вес: " + weight + "kg";
             totalPrice += price;
+            totalWeight += weight;
+
         } else {
             items += "\n" + name + " - " + price;
             totalPrice += price;
+            totalWeight += weight;
         }
     }
 
-    public void add(String name, int price) {
-        add(name, price, 1);
+    public void add(String name, int price, double weight) {
+        add(name, price, 1, weight);
     }
 
     public void clear() {
         items = "";
         totalPrice = 0;
+        totalWeight = 0;
     }
 
     public void print(String tittle) {
@@ -52,6 +57,7 @@ public class Basket {
         } else {
             System.out.println(items);
             System.out.println("Стоимость данной корзины равна: " + getTotalPrice());
+            System.out.println("Вес корзины: " + getTotalWeight());
         }
     }
 
@@ -66,6 +72,11 @@ public class Basket {
     public static int getCountBasket() {
         return countBasket;
     }
+
+    public double getTotalWeight() {
+        return totalWeight;
+    }
+
     // items типизирован стрингом
     // конкретный кекс - обьект
     // формочка - класс
--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,13 +1,13 @@
 public class Main2 {
     public static void main(String[] args) {
         Basket sergBasket = new Basket();
-        sergBasket.add("1 кг индейки", 700, 3);
-        sergBasket.add("Гранатовый сок", 300, 2);
-        sergBasket.add("Консервированный помидор", 250);
-        sergBasket.add("1 кг индейки", 700);
-        sergBasket.add("Пакет молока", 100);
-        sergBasket.add("Твикс", 65, 10);
-        sergBasket.add("1 кг индейки", 700, 2);
+        sergBasket.add("Индейки", 700, 3, 12.5);
+        sergBasket.add("Гранатовый сок", 300, 2, 13);
+        sergBasket.add("Консервированный помидор", 250, 12);
+        sergBasket.add("Индейки", 700, 1);
+        sergBasket.add("Пакет молока", 100, 12.4);
+        sergBasket.add("Твикс", 65, 2);
+        sergBasket.add("Индейки", 700, 1);
         sergBasket.print("Содержимое корзины:");
         sergBasket.clear();
         sergBasket.print("Содержимое корзины:");
@@ -15,16 +15,18 @@ public class Main2 {
         System.out.println();
 
         Basket petyaBasket = new Basket(5000);
-        petyaBasket.add("Миксер", 2150);
-        petyaBasket.add("Клавиатура", 3500);
+        petyaBasket.add("Миксер", 2150, 100);
+        petyaBasket.add("Клавиатура", 3500, 100);
         petyaBasket.print("Содержимое корзины: ");
 
         System.out.println();
 
-        Basket mashaBasket = new Basket("Стол", 10_000);
+        Basket mashaBasket = new Basket("Стол", 10_000, 20);
         mashaBasket.print("Содержимое корзины:");
 
         System.out.println("\nК-во корзин: " + Basket.getCountBasket());
+
+
     }
 }
     /*
